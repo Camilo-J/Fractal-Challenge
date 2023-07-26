cc6c730e6915d788245f5e304ad98013c7767d62
Camilo-J <josecamilo1902@outlook.com>
2023-07-26 14:29:26 -0500

    feat: add navlinks to each route of page

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,7 +1,17 @@
+import { SideLink } from "../navlink/navlink";
+import styles from "./style.module.css";
+
 export function SideBar() {
   return (
-    <div>
-      <p>testenado</p>
+    <div className={styles.sidebar}>
+      <nav className={styles.sidebar__links}>
+        <SideLink to={"/continents"} name={"Inicio"} />
+        <SideLink to={"/countries"} name={"Paises"} />
+        <SideLink to={"/continents"} name={"Continentes"} />
+        <SideLink to={"/continents"} name={"Lista 1"} />
+        <SideLink to={"/continents"} name={"Lista 2"} />
+        <SideLink to={"/continents"} name={"Lista 3"} />
+      </nav>
     </div>
   );
 }
