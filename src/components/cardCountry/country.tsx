c6fa2320db7b8cc15bf729fadf1533932285a61a
Camilo-J <josecamilo1902@outlook.com>
2023-07-27 11:02:44 -0500

    feat: add state to get image from pixabay

--- src/components/cardCountry/country.tsx
+++ src/components/cardCountry/country.tsx
@@ -1,33 +1,34 @@
+import { useEffect, useState } from "react";
 import styles from "./style.module.css";
+import { getPhotoCountry } from "../../services/pixabay";
 type CardCountryProps = {
-  countryImage: string;
-  flagImage: string;
   name: string;
-  continent: string;
+  continent: { name: string };
 };
 
-export function CardCountry({
-  countryImage,
-  flagImage,
-  name,
-  continent,
-}: CardCountryProps) {
+export function CardCountry({ name, continent }: CardCountryProps) {
+  const [countryImage, setCountryImage] = useState({
+    country: "",
+    flag: "",
+  });
+
+  useEffect(() => {
+    getPhotoCountry(name).then((data) => {
+      setCountryImage({
+        country: data.country.hits[0]?.webformatURL,
+        flag: data.flag.hits[0]?.webformatURL,
+      });
+    });
+  }, [name]);
+
   return (
     <div className={styles.cardContainer}>
-      <img
-        className={styles.mainImage}
-        src="../../../../public/palace-gd74aed6eb_1280.jpg"
-        alt=""
-      />
+      <img className={styles.mainImage} src={countryImage.country} alt="" />
       <div className={styles.cardBody}>
-        <img
-          className={styles.cardBody__Flag}
-          src="../../../../public/palace-gd74aed6eb_1280.jpg"
-          alt=""
-        />
+        <img className={styles.cardBody__Flag} src={countryImage.flag} alt="" />
         <div className={styles.cardBody__content}>
-          <h3>United Kingdom</h3>
-          <p>Europe</p>
+          <h3>{name}</h3>
+          <p>{continent?.name}</p>
         </div>
       </div>
     </div>
