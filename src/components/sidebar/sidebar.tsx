519c90e101e554e75eb7f57330f4fff1a9eeb67f
Camilo-J <josecamilo1902@outlook.com>
2023-07-28 11:11:59 -0500

    feat: delete useless routes

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -5,12 +5,7 @@ export function SideBar() {
   return (
     <div className={styles.sidebar}>
       <nav className={styles.sidebar__links}>
-        <SideLink to={"/continents"} name={"Inicio"} />
-        <SideLink to={"/countries"} name={"Paises"} />
-        <SideLink to={"/continents"} name={"Continentes"} />
-        <SideLink to={"/continents"} name={"Lista 1"} />
-        <SideLink to={"/continents"} name={"Lista 2"} />
-        <SideLink to={"/continents"} name={"Lista 3"} />
+        <SideLink to={"/countries"} name={"Inicio"} />
       </nav>
     </div>
   );
