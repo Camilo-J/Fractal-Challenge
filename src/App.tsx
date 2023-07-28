8a0217cf6129551b89d79a11803d78c3b06de067
Camilo-J <josecamilo1902@outlook.com>
2023-07-28 11:53:13 -0500

    feat: add state to open navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,23 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { SideBar } from "./components/sidebar/sidebar";
 import { Countries } from "./pages/countries/countries";
+import { useState } from "react";
 
 function App() {
+  const [open, setOpen] = useState(false);
+
+  function handleOpen() {
+    setOpen(!open);
+  }
   return (
     <section className="container">
-      <SideBar />
+      <img
+        onClick={handleOpen}
+        className="navbarIcon"
+        src="public/navbar.svg"
+        alt=""
+      />
+      <SideBar open={open} />
       <div className="container__pages">
         <Routes>
           <Route path="/countries" element={<Countries />} />
--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,9 +1,9 @@
 import { SideLink } from "../navlink/navlink";
 import styles from "./style.module.css";
 
-export function SideBar() {
+export function SideBar({ open }: { open: boolean }) {
   return (
-    <div className={styles.sidebar}>
+    <div className={`${styles.sidebar} ${open ? styles["sidebar--Open"] : ""}`}>
       <nav className={styles.sidebar__links}>
         <SideLink to={"/countries"} name={"Inicio"} />
       </nav>
