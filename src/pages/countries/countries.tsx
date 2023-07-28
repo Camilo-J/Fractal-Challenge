d640be612f1aa9a3e528936abd81c78ce128c111
Camilo-J <josecamilo1902@outlook.com>
2023-07-28 10:14:53 -0500

    feat: add methods to filter and show countries

--- src/pages/countries/countries.tsx
+++ src/pages/countries/countries.tsx
@@ -1,13 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { gql, useQuery } from "@apollo/client";
 import { CardCountry } from "../../components/cardCountry/country";
 import styles from "./style.module.css";
 import { useState } from "react";
+import { InfoCountry } from "../../components/infoCountry/infoCountry";
+import { useSearchParams } from "react-router-dom";
+import { Input } from "../../components/inputSearch/input";
+import { filterContinents, searchCountries } from "./helper";
 const ALL_COUNTRIES = gql`
   query {
     countries {
       code
       name
       continent {
+        code
         name
       }
     }
@@ -17,7 +23,7 @@ const ALL_COUNTRIES = gql`
 type Country = {
   code: string;
   name: string;
-  continent: { name: string };
+  continent: { name: string; code: string };
 };
 
 type AccuType = {
@@ -26,15 +32,24 @@ type AccuType = {
 
 export function Countries() {
   const [page, setPage] = useState(1);
+  const [filter, setFilter] = useState<string[]>([]);
+  const [searchParams, _setSearchParams] = useSearchParams();
+  const selectedCountry = searchParams.get("country");
+  const searchedCountry = searchParams.get("query");
   const result = useQuery(ALL_COUNTRIES);
-  const { data }: { data: { countries: [] } } = result;
+  let { data }: { data: { countries: Country[] } } = result;
   let option = 0;
   let pageNumber = 0;
-  if (data) {
+  if (data) pageNumber = Math.ceil(data?.countries.length / 9);
+
+  if (searchedCountry && data)
+    data = { countries: searchCountries(searchedCountry, data) };
+  if (filter.length > 0 && data) {
+    data = { countries: filterContinents(data, filter) };
     pageNumber = Math.ceil(data?.countries.length / 9);
   }
-  const cont: AccuType = {};
 
+  const cont: AccuType = {};
   const newdata = data?.countries.reduce((accu, current, index) => {
     if (index % 9 === 0) {
       option++;
@@ -48,25 +63,56 @@ export function Countries() {
     setPage(index + 1);
   };
 
+  function filters(params: string, clean: boolean, cleanAll: boolean) {
+    if (cleanAll) {
+      setFilter([]);
+      return;
+    }
+
+    if (clean) {
+      const newFilter = filter.filter((item) => item !== params);
+      setFilter(newFilter);
+      return;
+    }
+
+    if (params.split(" ").length === 1) {
+      setFilter([...filter, params]);
+    } else {
+      setFilter([...filter, ...params.split(" ")]);
+    }
+  }
   return (
-    <section>
-      <div className={styles.countriesContainer}>
-        {newdata &&
-          newdata[page]?.map(({ code, name, continent }: Country) => (
-            <CardCountry key={`id-${code}`} name={name} continent={continent} />
+    <section className={styles.wrapped}>
+      <Input onFilters={filters} />
+      <div className={styles.mainContainer}>
+        <div className={styles.countriesContainer}>
+          {newdata &&
+            newdata[page]?.map(({ code, name, continent }: Country) => (
+              <CardCountry
+                code={code}
+                key={`id-${code}`}
+                name={name}
+                continent={continent}
+              />
+            ))}
+        </div>
+        {selectedCountry && <InfoCountry />}
+      </div>
+      {!searchedCountry && (
+        <div className={styles.paginationContainer}>
+          {[...Array(pageNumber)].map((_x, index) => (
+            <button
+              className={`${
+                page === index + 1 ? styles["button--active"] : ""
+              }`}
+              onClick={() => handlePage(index)}
+              key={index}
+            >
+              {index + 1}
+            </button>
           ))}
-      </div>
-      <div className={styles.paginationContainer}>
-        {[...Array(pageNumber)].map((_x, index) => (
-          <button
-            className={`${page === index + 1 ? styles["button--active"] : ""}`}
-            onClick={() => handlePage(index)}
-            key={index}
-          >
-            {index + 1}
-          </button>
-        ))}
-      </div>
+        </div>
+      )}
     </section>
   );
 }
