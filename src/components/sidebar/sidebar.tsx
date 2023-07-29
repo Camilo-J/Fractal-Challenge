d2fd3a65947488b834721eacd66bc6f26a8fd6f5
Camilo-J <josecamilo1902@outlook.com>
2023-07-29 12:23:35 -0500

    feat: add sections for each continent in navbar

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -6,6 +6,11 @@ export function SideBar({ open }: { open: boolean }) {
     <div className={`${styles.sidebar} ${open ? styles["sidebar--Open"] : ""}`}>
       <nav className={styles.sidebar__links}>
         <SideLink to={"/countries"} name={"Inicio"} />
+        <SideLink to={"/europe"} name={"Europa"} />
+        <SideLink to={"/america"} name={"America"} />
+        <SideLink to={"/asia"} name={"Asia"} />
+        <SideLink to={"/africa"} name={"Africa"} />
+        <SideLink to={"/oceania"} name={"Oceania"} />
       </nav>
     </div>
   );
