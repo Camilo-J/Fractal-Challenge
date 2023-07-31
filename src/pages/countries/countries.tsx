7e9690ee9eb5f23ce3bb2cbea20e51c260f4626b
Camilo-J <josecamilo1902@outlook.com>
2023-07-31 10:09:48 -0500

    refactor: add component and use methods

--- src/pages/countries/countries.tsx
+++ src/pages/countries/countries.tsx
@@ -7,6 +7,9 @@ import { InfoCountry } from "../../components/infoCountry/infoCountry";
 import { useSearchParams } from "react-router-dom";
 import { Input } from "../../components/inputSearch/input";
 import { filterContinents, searchCountries } from "./helper";
+import { PaginationSection } from "../../components/pagination/pagination";
+import { GraphQLResponseCountries, Country } from "../../types/types";
+import { getNumberPages, paginateCountries } from "../../utils/utils";
 const ALL_COUNTRIES = gql`
   query {
     countries {
@@ -20,16 +23,6 @@ const ALL_COUNTRIES = gql`
   }
 `;
 
-type Country = {
-  code: string;
-  name: string;
-  continent: { name: string; code: string };
-};
-
-type AccuType = {
-  [key: number]: Country[];
-};
-
 export function Countries() {
   const [page, setPage] = useState(1);
   const [filter, setFilter] = useState<string[]>([]);
@@ -37,27 +30,18 @@ export function Countries() {
   const selectedCountry = searchParams.get("country");
   const searchedCountry = searchParams.get("query");
   const result = useQuery(ALL_COUNTRIES);
-  let { data }: { data: { countries: Country[] } } = result;
-  let option = 0;
+  let { data }: GraphQLResponseCountries = result;
   let pageNumber = 0;
-  if (data) pageNumber = Math.ceil(data?.countries.length / 9);
+  if (data) pageNumber = getNumberPages(data);
 
   if (searchedCountry && data)
     data = { countries: searchCountries(searchedCountry, data) };
   if (filter.length > 0 && data) {
     data = { countries: filterContinents(data, filter) };
-    pageNumber = Math.ceil(data?.countries.length / 9);
+    pageNumber = getNumberPages(data);
   }
 
-  const cont: AccuType = {};
-  const newdata = data?.countries.reduce((accu, current, index) => {
-    if (index % 9 === 0) {
-      option++;
-    }
-    accu[`${option}`] = accu[`${option}`] || [];
-    accu[`${option}`].push(current);
-    return accu;
-  }, cont);
+  const newdata = paginateCountries(data);
 
   const handlePage = (index: number) => {
     setPage(index + 1);
@@ -104,19 +88,11 @@ export function Countries() {
         {selectedCountry && <InfoCountry />}
       </div>
       {!searchedCountry && (
-        <div className={styles.paginationContainer}>
-          {[...Array(pageNumber)].map((_x, index) => (
-            <button
-              className={`${
-                page === index + 1 ? styles["button--active"] : ""
-              }`}
-              onClick={() => handlePage(index)}
-              key={index}
-            >
-              {index + 1}
-            </button>
-          ))}
-        </div>
+        <PaginationSection
+          handlePage={handlePage}
+          page={page}
+          pageNumber={pageNumber}
+        />
       )}
     </section>
   );
