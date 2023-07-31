b68314cc572432d8bded3e796b3df0f1a16ade5a
Camilo-J <josecamilo1902@outlook.com>
2023-07-31 09:48:09 -0500

    feat:  save  images url to avoid request to api

--- src/components/cardCountry/country.tsx
+++ src/components/cardCountry/country.tsx
@@ -4,33 +4,55 @@ import styles from "./style.module.css";
 import { getPhotoCountry } from "../../services/pixabay";
 import { useSearchParams } from "react-router-dom";
 import { CardCountryProps } from "../../types/types";
+import { GetImageProps, saveImageSessionStorage } from "./helper";
 
 export function CardCountry({ code, name, continent }: CardCountryProps) {
+  const data = sessionStorage.getItem(`countryImage-${name}`);
+  let images: GetImageProps = {
+    country: null,
+    flag: null,
+  };
+  if (data) images = JSON.parse(data);
+
   const [countryImage, setCountryImage] = useState({
     country: "",
     flag: "",
   });
   const [_searchParams, setSearchParams] = useSearchParams();
+
   useEffect(() => {
-    getPhotoCountry(name).then((data) => {
-      setCountryImage({
-        country: data.country.hits[0]?.webformatURL,
-        flag: data.flag.hits[0]?.webformatURL,
+    if (!data) {
+      getPhotoCountry(name).then((data) => {
+        saveImageSessionStorage({ name, data });
+        setCountryImage({
+          country: data.country.hits[0]?.webformatURL,
+          flag: data.flag.hits[0]?.webformatURL,
+        });
       });
-    });
-  }, [name]);
+    }
+  }, [name, data]);
 
   function showInfo() {
     setSearchParams(
-      `country=${code}&image=${countryImage.country}&flag=${countryImage.flag}`
+      `country=${code}&image=${images.country || countryImage.country}&flag=${
+        images.flag || countryImage.flag
+      }`
     );
   }
 
   return (
     <div onClick={showInfo} className={styles.cardContainer}>
-      <img className={styles.mainImage} src={countryImage.country} alt="" />
+      <img
+        className={styles.mainImage}
+        src={images.country || countryImage.country}
+        alt=""
+      />
       <div className={styles.cardBody}>
-        <img className={styles.cardBody__Flag} src={countryImage.flag} alt="" />
+        <img
+          className={styles.cardBody__Flag}
+          src={images.flag || countryImage.flag}
+          alt=""
+        />
         <div className={styles.cardBody__content}>
           <h3>{name}</h3>
           <p>{continent?.name}</p>
