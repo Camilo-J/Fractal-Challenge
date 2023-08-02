8b7e6513eb94fb34e9d7e63384756d48e6e6da27
Camilo-J <josecamilo1902@outlook.com>
2023-08-01 20:18:55 -0500

    refactor: move container to mainComponent

--- src/pages/asia/asia.tsx
+++ src/pages/asia/asia.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from "react";
-// import styles from "./style.module.css";
 import { ContainerCountries } from "../../components/containerCountries/containerCountries";
 
 // const GET_COUNTRIES = gql`
@@ -21,9 +20,5 @@ export function AsiaPage() {
   const handlePage = (index: number) => {
     setPage(index + 1);
   };
-  return (
-    <div>
-      <ContainerCountries page={page} handlePage={handlePage} />
-    </div>
-  );
+  return <ContainerCountries page={page} handlePage={handlePage} />;
 }
